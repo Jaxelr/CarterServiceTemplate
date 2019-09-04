b945a5d389d306429b6ce29389dca6b5f4529a13
jaxelr <jaxelr@gmail.com>
2019-09-04 10:03:46 -0400

    docs: added documentation to methods

--- Content/src/Extensions/ModuleExtensions.cs
+++ Content/src/Extensions/ModuleExtensions.cs
@@ -9,6 +9,13 @@ namespace CarterService.Extensions
 {
     public static class ModuleExtensions
     {
+        /// <summary>
+        /// Encapsulate execution of handler with the corresponding validation logic
+        /// </summary>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="res">An http response that will be populated</param>
+        /// <param name="handler">A func handler that will be validated and executed</param>
+        /// <returns></returns>
         public static Task ExecHandler<TOut>(this HttpResponse res, Func<TOut> handler)
         {
             try
@@ -31,12 +38,21 @@ namespace CarterService.Extensions
             }
         }
 
+        /// <summary>
+        /// Encapsulate execution of handler with the validation logic and storage on cache using the key provided
+        /// </summary>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="res">An http response that will be populated</param>
+        /// <param name="key">A string key that will be used to identify the request</param>
+        /// <param name="store">A cache store provided by the client</param>
+        /// <param name="handler">A func handler that will be validated and executed</param>
+        /// <returns></returns>
         public static Task ExecHandler<TOut>(this HttpResponse res, string key, Store store, Func<TOut> handler)
         {
             try
             {
                 var response = store.GetOrSetCache(key, () => handler());
-
+        
                 if (response == null)
                 {
                     res.StatusCode = 204;
@@ -53,6 +69,15 @@ namespace CarterService.Extensions
             }
         }
 
+        /// <summary>
+        /// Encapsulate execution of handler with the validation logic while binding and validating the http request
+        /// </summary>
+        /// <typeparam name="TIn"></typeparam>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="res">An http response that will be populated</param>
+        /// <param name="req">An http request that will be binded and validated</param>
+        /// <param name="handler">A func handler that will be validated and executed</param>
+        /// <returns></returns>
         public static Task ExecHandler<TIn, TOut>(this HttpResponse res, HttpRequest req, Func<TIn, TOut> handler)
         {
             try
@@ -83,6 +108,18 @@ namespace CarterService.Extensions
             }
         }
 
+        /// <summary>
+        /// Encapsulate execution of handler with the validation logic while binding,
+        /// validating the http request and storing on cache using the key provided
+        /// </summary>
+        /// <typeparam name="TIn"></typeparam>
+        /// <typeparam name="TOut"></typeparam>
+        /// <param name="res">An http response that will be populated</param>
+        /// <param name="req">An http request that will be binded and validated</param>
+        /// <param name="key">A string key that will be used to identify the request</param>
+        /// <param name="store">A cache store provided by the client</param>
+        /// <param name="handler">A func handler that will be validated and executed</param>
+        /// <returns></returns>
         public static Task ExecHandler<TIn, TOut>(this HttpResponse res, HttpRequest req, string key, Store store, Func<TIn, TOut> handler)
         {
             try

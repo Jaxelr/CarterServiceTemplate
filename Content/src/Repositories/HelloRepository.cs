1b542018992fd63645630f015abdf7f3c35e5d81
jaxelr <jaxelr@gmail.com>
2019-09-04 10:04:18 -0400

    refactor: added timestamp to helloRepo func for validation of cache

--- Content/src/Repositories/HelloRepository.cs
+++ Content/src/Repositories/HelloRepository.cs
@@ -2,6 +2,6 @@
 {
     public class HelloRepository : IHelloRepository
     {
-        public string SayHello(string name) => $"Hello world, your name is {name}";
+        public string SayHello(string name) => $"Hello world, your name is {name} the hour cached is {System.DateTime.Now}";
     }
 }

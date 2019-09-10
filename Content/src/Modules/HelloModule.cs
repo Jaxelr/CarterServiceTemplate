3496bea56535f953108fc6b8e7ebbe7fe901938a
jaxelr <jaxelr@gmail.com>
2019-09-10 10:41:41 -0400

    fix: removed unused props

--- Content/src/Modules/HelloModule.cs
+++ Content/src/Modules/HelloModule.cs
@@ -1,23 +1,15 @@
-﻿using CarterService.Cache;
-using CarterService.Repository;
-using CarterService.Extensions;
-using Carter;
+﻿using Carter;
 using Carter.Request;
+using CarterService.Cache;
+using CarterService.Extensions;
+using CarterService.Repository;
 
 namespace CarterService.Modules
 {
     public class HelloModule : CarterModule
     {
-#pragma warning disable IDE0052 // Remove unread private members
-        private readonly IHelloRepository repository;
-        private readonly Store store;
-#pragma warning restore IDE0052 // Remove unread private members
-
         public HelloModule(IHelloRepository repository, Store store)
         {
-            this.repository = repository;
-            this.store = store;
-
             Get<GetHello>("/Hello/{name}", (req, res, routeData) =>
             {
                 string name = routeData.As<string>("name");

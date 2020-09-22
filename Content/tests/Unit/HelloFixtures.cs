ccca7f91bc56b547251ddf520699c7d5c5855a7d
Jaxel Rojas <jaxelr@gmail.com>
2020-09-22 18:06:23 -0400

    tests: removing unused tests

--- Content/tests/Unit/HelloFixtures.cs
+++ Content/tests/Unit/HelloFixtures.cs
@@ -2,7 +2,6 @@
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using CarterService;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server.Features;

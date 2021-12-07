f3a3102958664422cec919d2eeb37271df963a84
Jaxel Rojas <jaxelr@gmail.com>
2021-12-07 10:17:44 -0400

    refactor: namespace simplification on tests

--- Content/tests/Unit/HelloFixtures.cs
+++ Content/tests/Unit/HelloFixtures.cs
@@ -7,42 +7,41 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
-namespace CarterService.Tests.Unit
-{
-    public class HelloModuleFixtures : IDisposable
-    {
-        private readonly HttpClient client;
+namespace CarterService.Tests.Unit;
 
-        public HelloModuleFixtures()
-        {
-            var server = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder => builder.ConfigureServices
-            (
+public class HelloModuleFixtures : IDisposable
+{
+    private readonly HttpClient client;
+
+    public HelloModuleFixtures()
+    {
+        var server = new WebApplicationFactory<Program>()
+        .WithWebHostBuilder(builder => builder.ConfigureServices
+        (
                 /// Mock, if needed
                 services => services.AddSingleton<IHelloRepository, HelloRepository>())
-            );
+        );
 
-            client = server.CreateClient();
-        }
+        client = server.CreateClient();
+    }
 
-        public void Dispose()
-        {
-            client?.Dispose();
-            GC.SuppressFinalize(this);
-        }
+    public void Dispose()
+    {
+        client?.Dispose();
+        GC.SuppressFinalize(this);
+    }
 
-        [Fact]
-        public async Task Hello_module_get_hello_world()
-        {
-            //Arrange
-            const string name = "myUser";
+    [Fact]
+    public async Task Hello_module_get_hello_world()
+    {
+        //Arrange
+        const string name = "myUser";
 
-            //Act
-            var res = await client.GetAsync($"/hello/{name}");
+        //Act
+        var res = await client.GetAsync($"/hello/{name}");
 
-            //Assert
-            Assert.Equal(HttpStatusCode.OK, res.StatusCode);
-            Assert.Contains(name, await res.Content.ReadAsStringAsync());
-        }
+        //Assert
+        Assert.Equal(HttpStatusCode.OK, res.StatusCode);
+        Assert.Contains(name, await res.Content.ReadAsStringAsync());
     }
 }

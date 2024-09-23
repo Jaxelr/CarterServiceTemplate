69deae969a763a892ff3eae8d0184d8d72292461
Jaxel Rojas <jaxelr@gmail.com>
2024-09-23 08:58:55 -0400

    refactor: comply with nullability enableness

--- Content/src/Extensions/TypeExtensions.cs
+++ Content/src/Extensions/TypeExtensions.cs
@@ -22,7 +22,7 @@ public static class TypeExtensions
     internal static Type GetAnyElementType(this Type type)
     {
         if (type.IsArray)
-            return type.GetElementType();
+            return type.GetElementType()!;
 
         if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEnumerable<>))
             return type.GetGenericArguments()[0];
